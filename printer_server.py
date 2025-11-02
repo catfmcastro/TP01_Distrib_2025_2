04c08f22546e12975f19fe40f89e52fa3cbfc69d
André Menezes <60123295+AndreMenezes03@users.noreply.github.com>
2025-11-02 19:25:55 -0300

    Humanizado

--- printer_server.py
+++ printer_server.py
@@ -9,13 +9,9 @@ import printing_pb2
 import printing_pb2_grpc
 
 class PrintingServiceServicer(printing_pb2_grpc.PrintingServiceServicer):
-    """
-    Implementa o serviço gRPC para o servidor de impressão.
-    """
+    # Implementa o serviço gRPC para o servidor de impressão.
     def SendToPrinter(self, request, context):
-        """
-        Recebe uma requisição de impressão, simula o trabalho e retorna uma confirmação.
-        """
+        # Recebe uma requisição de impressão, simula o trabalho e retorna uma confirmação.
         client_id = request.client_id
         timestamp = request.lamport_timestamp
         message = request.message_content
@@ -36,9 +32,7 @@ class PrintingServiceServicer(printing_pb2_grpc.PrintingServiceServicer):
         )
 
 def serve():
-    """
-    Inicia o servidor gRPC e o mantém em execução.
-    """
+    # Inicia o servidor gRPC e o mantém em execução.
     port = '50051'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     printing_pb2_grpc.add_PrintingServiceServicer_to_server(PrintingServiceServicer(), server)
